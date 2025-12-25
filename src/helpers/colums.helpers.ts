0662eb53461576fd921241af1d3ca6782cb76b54
solomonadzape95 <adzapesolomon@gmail.com>
2025-12-25 13:15:57 +0100

    feat: added column helper functions for importing timestamp columns

---
+++ src/helpers/colums.helpers.ts
@@ -0,0 +1,5 @@
+import { timestamp } from "drizzle-orm/pg-core"
+export const timestamps = {
+    updatedAt: timestamp('updated_at').defaultNow().notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull(),
+}
